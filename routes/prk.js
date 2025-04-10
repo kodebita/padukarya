ef5f510da88db47cb6df05dee04c583c73232a81
kodebita <kodebita@gmail.com>
2025-04-10 11:36:04 +0700

    progress prk lampiran upload and delete

--- routes/prk.js
+++ routes/prk.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const Tokens = require('csrf')
+const Tokens = require('csrf');
+const multer  = require('multer');
 const { body, validationResult } = require('express-validator');
 const formatError = require('../helper/error-formatter');
+const fs = require('fs');
+const path = require('path');
 
 // generate token
 const tokens = new Tokens();
@@ -15,6 +18,9 @@ const PrkJasa = require("../models/prkJasa");
 const PrkLampiran = require("../models/prkLampiran");
 const PrkCatatan = require("../models/prkCatatan");
 const Material = require("../models/material");
+
+// get upload helper
+const upload = require('../helper/upload-helper');
 
 router.get("/", async (req, res) => {
   try {
@@ -654,6 +660,116 @@ router.get("/:id/lampiran", async (req, res) => {
       title: "Lampiran PRK",
       prk: prk,
       lampirans: lampirans,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.get('/:id/lampiran/baru', async (req, res) => {
+  try {
+    const token = tokens.create(secret);
+    const prk = await Prk.findOne({ _id: req.params.id }).lean();
+
+    res.render("prk/detail/lampiran/create", {
+      title: "Lampiran PRK",
+      prk: prk,
+      token: token,
+      errors: req.flash('errors')[0] || {},
+      old: req.flash('old')[0] || {},
+      toast: req.flash('toast')[0] || false,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post('/:id/lampiran/baru', async (req, res, next) => {
+  try {
+    const id = req.params.id;
+
+    upload.single('file')(req, res, async function (err) {
+      if (err instanceof multer.MulterError) {
+        if (err.code === 'LIMIT_FILE_SIZE') {
+          req.flash('errors', { file: 'Ukuran file maksimal 15MB' });
+        } else {
+          req.flash('errors', { file: 'Terjadi kesalahan unggah file' });
+        }
+        
+        return res.redirect('/prk/'+id+'/lampiran/baru');
+      } else if (err) {
+        req.flash('errors', { file: err.message });
+        return res.redirect('/prk/'+id+'/lampiran/baru');
+      }
+  
+      if (!req.file) {
+        req.flash('errors', { file: 'File wajib diunggah' });
+        req.flash('toast', req.flash('toast', {
+          success: false,
+          message: 'Terjadi Kesalahan'
+        }));
+        return res.redirect('/prk/'+id+'/lampiran/baru');
+      }
+
+      // write to mongo
+      const prkLampiran = new PrkLampiran({
+        prk_id: req.params.id,
+        nama_file: req.file.originalname,
+        url_file: req.file.filename,
+        type_file: req.file.mimetype,
+        size_file: req.file.size,
+        created_at: new Date().toISOString()
+      });
+  
+      await prkLampiran.save();
+  
+      req.flash('toast', req.flash('toast', {
+        success: true,
+        message: 'Lampiran berhasil ditambahkan'
+      }));
+
+      return res.redirect('/prk/'+id+'/lampiran');
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.get('/:id/lampiran/:lampiranId/hapus', async (req, res) => {
+  try{
+    const id = req.params.id;
+    const lampiranId = req.params.lampiranId;
+
+    const lampiran = await PrkLampiran.findOne({ _id: lampiranId }).lean();
+    if (!lampiran) {
+      req.flash('toast', {
+        success: false,
+        message: 'Lampiran tidak ditemukan'
+      });
+
+      return res.redirect('/prk/'+id+'/lampiran');
+    }
+  
+    const fullPath = path.join(__dirname, '../public/uploads/lampiran/', lampiran.url_file);
+    
+    fs.unlink(fullPath, async (err) => {
+      if (err) {
+        req.flash('toast', {
+          success: false,
+          message: 'Terjadi kesalahan'
+        });
+
+        return res.redirect('/prk/'+id+'/lampiran');
+      }
+
+      // delete from mongo
+      await PrkLampiran.findOneAndDelete({ _id: lampiranId });
+  
+      req.flash('toast', {
+        success: true,
+        message: 'Lampiran berhasil dihapus'
+      });
+      return res.redirect('/prk/'+id+'/lampiran');
     });
   } catch (error) {
     res.status(500).json({ error: error.message });
