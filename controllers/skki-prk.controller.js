a00044a9f8e9cdcf498a2f66b2d51c17f238ad4a
kodebita <kodebita@gmail.com>
2025-04-14 14:16:38 +0700

    progress skki prk

--- controllers/skki-prk.controller.js
+++ controllers/skki-prk.controller.js
@@ -1,3 +1,10 @@
+const { validationResult } = require('express-validator');
+const formatError = require('../helper/error-formatter');
+const Tokens = require('csrf');
+
+const tokens = new Tokens();
+const secret = tokens.secretSync();
+
 const Skki = require("../models/skki");
 const Prk = require("../models/prk");
 const PrkMaterial = require("../models/prkMaterial");
@@ -41,6 +48,100 @@ async function getSkkiPrk(req, res) {
   }
 }
 
+async function createSkkiPrk(req, res) {
+  try {
+    const skki = await Skki.findOne({ _id: req.params.id }).lean();
+
+    res.render("skki/detail/prk/create", {
+      title: "Tambah PRK SKKI",
+      skki: skki,
+      token: tokens.create(secret),
+      errors: req.flash('errors')[0] || {},
+      old: req.flash('old')[0] || {}
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+async function storeSkkiPrk(req, res) {
+  try{
+    const skkiId = req.params.id;
+
+    const skki = await Skki.findOne({ _id: skkiId }).lean();
+    if(!skki) {
+      req.flash('errors', { skki_id: 'SKKI tidak ditemukan' });
+      return res.redirect('/skki/'+skkiId+'/prk/baru');
+    }
+    
+    const { 
+      prk_id,
+      token
+    } = req.body;
+
+    if(!tokens.verify(secret, token)) {
+      throw new Error('invalid token!')
+    }
+
+    const prk = await Prk.findOne({ _id: prk_id }).lean();
+    if(!prk) {
+      req.flash('errors', { prk_id: 'PRK tidak ditemukan' });
+      req.flash('old', req.body);
+      return res.redirect('/skki/' + id + '/prk/baru');
+    }
+
+    await Prk.updateOne({ _id: prk._id }, { $set: { prk_skki_id: skki._id } });
+
+    req.flash('toast', req.flash('toast', {
+      success: true,
+      message: 'PRK berhasil ditambahkan'
+    }));
+
+    res.redirect("/skki/"+skkiId+"/prk");
+  } catch(error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+async function deleteSkkiPrk(req, res) {
+  try {
+    const skkiId = req.params.id;
+    const prkId = req.params.prkId;
+
+    const updateData = {
+      prk_skki_id: null,
+      updated_at: new Date().toISOString(), // auto-update updated_at
+    };
+
+    const updatedPrk = await Prk.findByIdAndUpdate(prkId, updateData, {
+      new: true, // return updated document
+      runValidators: true,
+    });
+
+    if (!updatedPrk) {
+      req.flash('toast', {
+        success: false,
+        message: 'PRK tidak ditemukan'
+      });
+      res.redirect("/skki/" + skkiId + "/prk");
+    }
+
+    // success
+    req.flash('toast', req.flash('toast', {
+      success: true,
+      message: 'PRK berhasil dihapus dari SKKI'
+    }));
+
+    // redirect to the same page
+    res.redirect("/skki/" + skkiId + "/prk");
+  } catch(error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
 module.exports = {
   getSkkiPrk,
+  createSkkiPrk,
+  storeSkkiPrk,
+  deleteSkkiPrk
 };
